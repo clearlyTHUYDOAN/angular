7d6aa398f66fdd9036ac50702509979a1d0d9882
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2016-12-27 14:10:42 -0500

    start angular notes

---
+++ app.js
@@ -0,0 +1,8 @@
+// Use Angular module function to construct module.
+// Takes two arguments: 
+    // 1) string referring to app name defined in ng-app directive. 
+    // 2) Array of module dependencies (empty in example).
+// Note that we are storing our module in a variable. Will use it later to reference the module.
+
+angular
+    .module("myApp",[]);
